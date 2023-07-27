e3393c9c5780183530f4702b851e674058d52967
Jaimesomohano <jaime.somohano@aspentech.com>
2023-07-27 23:46:25 +0000

    Create the project

---
+++ UftDeveloperTest.cs
@@ -0,0 +1,39 @@
+﻿using HP.LFT.SDK;
+using HP.LFT.Verifications;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ACM_UFT
+{
+    [TestClass]
+    public class UftDeveloperTest : UnitTestClassBase<UftDeveloperTest>
+    {
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            GlobalSetup(context);
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            GlobalTearDown();
+        }
+    }
+}
